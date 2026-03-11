943653cb5e8b6e627306fb928696efd97f4b84ab
Milly <dauchenberg4@nait.ca>
2026-03-11 17:58:45 -0600

    Created the Database Models. Two tables, Server and Metric.

--- backend/metrics/models.py
+++ backend/metrics/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+class Server(models.Model):
+    name = models.CharField(max_length=100)
+    ip_address = models.GenericIPAddressField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Metric(models.Model):
+    server = models.ForeignKey(Server, on_delete=models.CASCADE)
+    cpu_usage = models.FloatField()
+    ram_usage = models.FloatField()
+    disk_usage = models.FloatField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.server.name} - {self.timestamp}"
